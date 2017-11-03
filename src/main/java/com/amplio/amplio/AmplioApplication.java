f765a9adcf4a1907d47d731d817d590fcd5596d8
Mrunal Patel <mkpatel@cs.stonybrook.edu>
2017-11-03 19:20:53 -0400

    base spring project setup

---
+++ src/main/java/com/amplio/amplio/AmplioApplication.java
@@ -0,0 +1,12 @@
+package com.amplio.amplio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmplioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmplioApplication.class, args);
+	}
+}
