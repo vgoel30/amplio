62527bb3c04d0fb94c7ed2a83e547adb604d6327
Marisa <depasquale.marisa@gmail.com>
2017-11-03 21:51:41 -0400

    Updated Statistics class

--- src/main/java/com/amplio/amplio/models/Statistics.java
+++ src/main/java/com/amplio/amplio/models/Statistics.java
@@ -1,4 +1,32 @@
 package com.amplio.amplio.models;
+import java.util.List;
 
 public class Statistics {
+    private List<Artist> topArtists;
+    private List<Song> topSongs;
+
+    public Statistics(){
+        this.topArtists = null;
+        this.topSongs = null;
+    }
+
+    public void addTopArtist(Artist artist){
+        this.topArtists.add(artist);
+    }
+
+    public void addTopSong(Song song){
+        this.topSongs.add(song);
+
+    }
+
+    public void delTopArtist(Artist artist){
+        this.topArtists.remove(artist);
+    }
+
+    public void delTopSong(Song song){
+        this.topSongs.remove(song);
+
+    }
+
 }
+
