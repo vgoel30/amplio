bc827f41940d4f840d890a97880215e13a03377f
Rishab Chhabra <rishab.chhabra@stonybrook.edu>
2017-11-04 02:22:49 -0400

    Added SongCollection class

---
+++ src/main/java/com/amplio/amplio/models/SongCollection.java
@@ -0,0 +1,28 @@
+package com.amplio.amplio.models;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public class SongCollection {
+
+    @NotNull
+    private String title;
+    @NotNull
+    private List<Song> songs;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+}
