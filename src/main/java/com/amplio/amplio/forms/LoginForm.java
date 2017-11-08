387f29c8414ea84ed6d2c1cd8271a1e21738a7a6
vgoel30 <varun.goel@stonybrook.edu>
2017-11-08 00:26:29 -0500

    Removed redundant imports

--- src/main/java/com/amplio/amplio/forms/LoginForm.java
+++ src/main/java/com/amplio/amplio/forms/LoginForm.java
@@ -1,12 +1,6 @@
 package com.amplio.amplio.forms;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
-@Builder
 public class LoginForm {
     private String userName;
     private String password;
