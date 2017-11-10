e1d36ea9fce4fe7a955c10fe55a85076f8e669e2
Rishab Chhabra <rishab.chhabra@stonybrook.edu>
2017-11-09 21:23:53 -0500

    Re-reformatted code and fixed code formatting settings

--- src/main/java/com/amplio/amplio/forms/RegisterForm.java
+++ src/main/java/com/amplio/amplio/forms/RegisterForm.java
@@ -1,29 +1,29 @@
 package com.amplio.amplio.forms;
 
 public class RegisterForm {
-    private String firstName;
-    private String lastName;
-    private String username;
-    private String email;
-    private String password;
+  private String firstName;
+  private String lastName;
+  private String username;
+  private String email;
+  private String password;
 
-    public String getFirstName() {
-        return firstName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public String getUserName() {
-        return username;
-    }
+  public String getUserName() {
+    return username;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 }
