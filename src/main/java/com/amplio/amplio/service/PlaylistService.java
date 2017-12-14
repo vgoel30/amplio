ec9ea2e137f311e449fc912952c780ef322dc9de
Rishab Chhabra <rishab.chhabra@stonybrook.edu>
2017-12-14 14:06:23 -0500

    Query only public playlists. fixed more null lists

--- src/main/java/com/amplio/amplio/service/PlaylistService.java
+++ src/main/java/com/amplio/amplio/service/PlaylistService.java
@@ -31,7 +31,7 @@ public class PlaylistService {
     User currentUser = (User) session.getAttribute(Constants.SESSION_USER);
 
     if(currentUser != null) {
-      playlists = playlistRepository.findTop10PlaylistsByTitleContaining(query);
+      playlists = playlistRepository.findTop10PlaylistsByTitleContainingAndPublic(query);
     }
 
     return playlists;
@@ -115,7 +115,7 @@ public class PlaylistService {
     Set<Playlist> generatedPlaylists = null;
 
     if(amplioUser != null) {
-      generatedPlaylists = playlistRepository.findPlaylistsByOwner(amplioUser);
+      generatedPlaylists = playlistRepository.findPlaylistsByOwnerAndPublic(amplioUser);
     }
     return generatedPlaylists;
   }
@@ -126,7 +126,7 @@ public class PlaylistService {
     Set<Playlist> playlists = null;
 
     if(user != null && playlistOwner != null) {
-      playlists = playlistRepository.findPlaylistsByOwner(playlistOwner);
+      playlists = playlistRepository.findPlaylistsByOwnerAndPublic(playlistOwner);
     }
 
     return playlists;
