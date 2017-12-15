965c90e1ede54e976f6c9f3f8e1bd27041e99f7e
Rishab Chhabra <rishab.chhabra@stonybrook.edu>
2017-12-15 03:29:30 -0500

    limit playlist search results

--- src/main/java/com/amplio/amplio/repository/PlaylistRepository.java
+++ src/main/java/com/amplio/amplio/repository/PlaylistRepository.java
@@ -14,10 +14,10 @@ import java.util.Set;
 public interface PlaylistRepository extends CrudRepository<Playlist, Integer> {
   Playlist getPlaylistById(Integer playlistId);
 
-  @Query(value = "Select * from playlist where owner_id = :id and is_public = true ", nativeQuery = true)
+  @Query(value = "Select * from playlist where owner_id = :id and is_public = true limit 10", nativeQuery = true)
   Set<Playlist> findPlaylistsByOwnerAndPublic(@Param("id") Integer ownerId);
 
-  @Query(value = "Select * from playlist where title like %:query% and is_public = true ", nativeQuery = true)
+  @Query(value = "Select * from playlist where title like %:query% and is_public = true limit 10", nativeQuery = true)
   List<Playlist> findTop10PlaylistsByTitleContainingAndPublic(@Param("query") String playlistName);
 
   Set<Playlist> findPlaylistsByOwner(User user);
